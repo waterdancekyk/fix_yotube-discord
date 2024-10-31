2f852040a59d0e8b4616dda29592e6ed824dc58d
waterdancekyk <iaros.rijkow2015@mail.ru>
2024-10-31 09:06:22 +0300

    first commit

--- service_goodbye_discord.bat
+++ service_goodbye_discord.bat
@@ -3,14 +3,19 @@ chcp 65001 >nul
 :: 65001 - UTF-8
 
 :: Admin rights check
+echo Предупреждение: Данный сервис работает ТОЛЬКО ВМЕСТЕ С СЕРВИСОМ GoodbyeDPI
+echo Нажмите любую клавишу, чтобы продолжить создание сервиса.
+pause
+
+:: Admin rights check
 echo Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
 echo Нажмите любую клавишу, чтобы продолжить создание сервиса.
 pause
 
 set BIN=%~dp0bin\
-set ARGS=--wf-tcp=443 --wf-udp=443,50000-65535 ^
---filter-udp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
---filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
+set ARGS=--wf-tcp=443 --wf-udp=443,50000-50100 ^
+--filter-udp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
+--filter-udp=50000-50100 --ipset=\"%~dp0ipset-discord.txt\" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
 
 set SRVCNAME=zapret
